c39abb6293f3529042a4c1f287d487286f04de89
knyazann <knyazeva-anna00@bk.ru>
2022-05-10 18:26:29 +0300

    rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User do
+    let(:user) { described_class.new }
+
+    it "is valid with valid attributes" do
+        user.login = "qwerty"
+        user.password = "qwerty"
+        expect(user).to be_valid
+    end
+
+    describe "Associations" do
+        it { should have_many(:documents) }
+      end
+
+
+end
